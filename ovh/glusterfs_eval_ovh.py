3446585cfbd158b620329bbd1731af77e91e452d
ntlinh16 <ntlinh16@gmail.com>
2022-02-18 10:36:12 +0100

    refactor: refactor to use configurators

--- ovh/glusterfs_eval_ovh.py
+++ ovh/glusterfs_eval_ovh.py
@@ -4,14 +4,12 @@ import random
 from cloudal.utils import get_logger, execute_cmd, parse_config_file, ExecuteCommandException
 from cloudal.action import performing_actions
 from cloudal.provisioner import ovh_provisioner
-from cloudal.configurator import packages_configurator, CancelException
-from cloudal.experimenter import create_paramsweeper, define_parameters, get_results
+from cloudal.configurator import filebench_configurator, glusterfs_configurator, CancelException
+from cloudal.experimenter import create_paramsweeper, define_parameters, get_results, is_node_active, delete_ovh_nodes
 
 from execo_engine import slugify
 
 logger = get_logger()
-
-
 
 class glusterfs_eval_ovh(performing_actions):
     def __init__(self):
@@ -35,77 +33,25 @@ class glusterfs_eval_ovh(performing_actions):
                     remote_result_files=['/tmp/results/*'],
                     local_result_dir=self.configs['exp_env']['results_dir'])
 
-    def run_mailserver(self, gluster_hosts, duration, n_client):
-        if n_client == 100:
-            n_hosts = 1
-        else:
-            n_hosts = n_client
-
-        hosts = random.sample(gluster_hosts, n_hosts)
-        logger.info('Dowloading Filebench configuration file')
-        cmd = 'wget https://raw.githubusercontent.com/filebench/filebench/master/workloads/varmail.f -P /tmp/ -N'
-        execute_cmd(cmd, hosts)
-
-        logger.info('Editing the configuration file')
-        cmd = 'sed -i "s/tmp/tmp\/dc-$(hostname)/g" /tmp/varmail.f'
-        execute_cmd(cmd, hosts)
-        cmd = 'sed -i "s/run 60/run %s/g" /tmp/varmail.f' % duration
-        execute_cmd(cmd, hosts)
-        cmd = 'sed -i "s/name=bigfileset/name=bigfileset-$(hostname)/g" /tmp/varmail.f'
-        execute_cmd(cmd, hosts)
-        cmd = 'sed -i "s/meandirwidth=1000000/meandirwidth=1000/g" /tmp/varmail.f'
-        execute_cmd(cmd, hosts)
-        if n_client != 100:
-            cmd = 'sed -i "s/nthreads=16/nthreads=32/g" /tmp/varmail.f'
-            execute_cmd(cmd, hosts)
-
-        logger.info('Clearing cache ')
-        cmd = 'rm -rf /tmp/dc-$(hostname)/bigfileset'
-        execute_cmd(cmd, hosts)
-        cmd = 'sync; echo 3 > /proc/sys/vm/drop_caches'
-        execute_cmd(cmd, hosts)
-
-        logger.info('hosts = %s' % hosts)
-        logger.info('Running filebench in %s second' % duration)
-        cmd = 'setarch $(arch) -R filebench -f /tmp/varmail.f > /tmp/results/filebench_$(hostname)'
-        execute_cmd(cmd, hosts)
-        return True, hosts
-
-    def run_benchmark(self, comb, glusterfs_hosts):
+    def run_benchmark(self, comb, hosts, gluster_mountpoint):
         benchmark = comb['benchmarks']
         logger.info('--------------------------------------')
         logger.info("3. Starting benchmark: %s" % benchmark)
-        if benchmark == "mailserver":
-            return self.run_mailserver(glusterfs_hosts, comb['duration'], comb['n_client'])
+        gluster_mountpoint = 'mnt\/glusterd-$(hostname)'
+        filebench_hosts = random.sample(hosts, comb['n_clients'])
+        configurator = filebench_configurator()
+        if benchmark == 'mailserver':
+            is_finished = configurator.run_mailserver(filebench_hosts, gluster_mountpoint, comb['duration'], comb['n_threads'])
+            return is_finished, filebench_hosts
 
-    def deploy_glusterfs(self, hosts, gluster_volume_name):
+    def deploy_glusterfs(self, indices, gluster_mountpoint, gluster_volume_name):
         logger.info('--------------------------------------')
         logger.info("2. Deploying GlusterFS")
-        logger.info('Creating volumes on hosts')
-        volume_path = '/tmp/glusterd/volume'
-        cmd = 'mkdir -p %s' % volume_path
-        execute_cmd(cmd, hosts)
-
-        volume_params = list()
-        for index, node in enumerate(self.data_nodes):
-            volume_params.append("gluster-%s.%s.local:%s" % (index, node['name'], volume_path))
-        volume_params = " ".join(volume_params)
-
-        cmd ='gluster --mode=script volume create %s replica 3 %s' % (gluster_volume_name, volume_params)
-        execute_cmd(cmd, self.data_nodes[0]['ipAddresses'][0]['ip'])
-        
-        logger.info('Starting volumes on hosts')
-        cmd = 'gluster --mode=script volume start %s' % gluster_volume_name
-        execute_cmd(cmd, self.data_nodes[0]['ipAddresses'][0]['ip'])
-
-        cmd = ''' mkdir -p /mnt/glusterd-$(hostname) &&
-                  mount -t glusterfs gluster-0:/%s /mnt/glusterd-$(hostname)''' % gluster_volume_name
-        execute_cmd(cmd, hosts)
-        logger.info("Finish deploying glusterfs")
-        return True
+        configurator = glusterfs_configurator()
+        return configurator.deploy_glusterfs(self.hosts, indices, gluster_mountpoint, gluster_volume_name)
 
 
-    def clean_exp_env(self, hosts, gluster_volume_name):
+    def clean_exp_env(self, hosts, gluster_mountpoint, gluster_volume_name):
         logger.info('1. Cleaning experiment environment ')
         if len(hosts) > 0:
             logger.info('Delete all files in /tmp/results folder on %s glusterfs nodes' % len(hosts))
@@ -114,14 +60,14 @@ class glusterfs_eval_ovh(performing_actions):
             
             logger.info('Delete all data on %s glusterfs nodes' % len(hosts))
             for host in hosts:
-                cmd = "mount | grep /mnt/glusterd-$(hostname)"
+                cmd = "mount | grep %s" % gluster_mountpoint
                 _, r = execute_cmd(cmd, host)
                 is_mount = r.processes[0].stdout.strip()
 
                 if is_mount:
                     execute_cmd(cmd, host)
-                    cmd = '''umount /mnt/glusterd-$(hostname) &&
-                            rm -rf /mnt/glusterd-$(hostname)/* '''
+                    cmd = '''umount %s &&
+                            rm -rf %s/* ''' % (gluster_mountpoint, gluster_mountpoint)
                     execute_cmd(cmd, host)
 
             logger.info('Delete all volumes on %s glusterfs nodes' % len(hosts))
@@ -134,18 +80,20 @@ class glusterfs_eval_ovh(performing_actions):
             cmd = 'rm -rf /tmp/glusterd/*'
             execute_cmd(cmd, hosts)
 
-    def run_exp_workflow(self, comb, sweeper, gluster_volume_name):
+    def run_exp_workflow(self, comb, sweeper, gluster_mountpoint, gluster_volume_name):
         comb_ok = False
         try:
             logger.info('=======================================')
             logger.info('Performing combination: ' + slugify(comb))
 
-            self.clean_exp_env(self.data_hosts, gluster_volume_name)
-            glusterfs = self.deploy_glusterfs(self.data_hosts, gluster_volume_name)
+            self.clean_exp_env(self.data_hosts, gluster_mountpoint, gluster_volume_name)
+            # Get indices to select the number of GlusterFS nodes from list of hosts
+            indices = random.sample(range(len(self.data_hosts)), comb['n_nodes_per_dc'] * comb['n_dc'])
+            glusterfs, hosts = self.deploy_glusterfs(indices, gluster_mountpoint, gluster_volume_name)
             if glusterfs:
-                is_finished, hosts = self.run_benchmark(comb, self.data_hosts)
+                is_finished, result_hosts = self.run_benchmark(comb, hosts, gluster_mountpoint)
                 if is_finished:
-                    self.save_results(comb, hosts)
+                    self.save_results(comb, result_hosts)
                     comb_ok = True
             else:
                 raise CancelException("Cannot deploy glusterfs")
@@ -162,51 +110,13 @@ class glusterfs_eval_ovh(performing_actions):
             logger.info('%s combinations remaining\n' % len(sweeper.get_remaining()))
         return sweeper
 
-    def install_filebench(self, hosts):
-        logger.info('Installing Filebench')
-        configurator = packages_configurator()
-        configurator.install_packages(["build-essential", "bison", "flex", "libtool"], hosts)
-
-        cmd = "wget https://github.com/filebench/filebench/archive/refs/tags/1.5-alpha3.tar.gz -P /tmp/ -N"
-        execute_cmd(cmd, hosts)
-        cmd = "tar -xf /tmp/1.5-alpha3.tar.gz --directory /tmp/"
-        execute_cmd(cmd, hosts)
-        cmd = '''cd /tmp/filebench-1.5-alpha3/ &&
-                 libtoolize &&
-                 aclocal &&
-                 autoheader &&
-                 automake --add-missing &&
-                 autoconf &&
-                 ./configure &&
-                 make &&
-                 make install'''
-        execute_cmd(cmd, hosts)
-
-    def install_gluster(self, hosts):
-        logger.info('Installing GlusterFS')
-        configurator = packages_configurator()
-        configurator.install_packages(["glusterfs-server"], hosts)
-        
-        cmd = 'systemctl start glusterd'
-        execute_cmd(cmd, hosts)
-
-        gluster_configuration = list()
-        for index, node in enumerate(self.data_nodes):
-            gluster_configuration.append("%s gluster-%s.%s.local gluster-%s " % (
-                                            node['ipAddresses'][0]['ip'], index, node['name'], index))
-        gluster_configuration = "\n".join(gluster_configuration)
-        cmd = "echo '%s' >> /etc/hosts" % gluster_configuration
-        execute_cmd(cmd, hosts)
-
-        for index, _ in enumerate(self.data_nodes):
-            cmd = 'gluster peer probe gluster-%s' % index
-            execute_cmd(cmd, self.data_nodes[0]['ipAddresses'][0]['ip'])
-
-
     def config_host(self):
         logger.info("Starting configuring nodes")
-        self.install_gluster(self.hosts)
-        self.install_filebench(self.hosts)
+        configurator = filebench_configurator()
+        configurator.install_filebench(self.hosts)
+        
+        configurator = glusterfs_configurator()
+        configurator.install_glusterfs(self.hosts)
         logger.info("Finish configuring nodes")
 
     def setup_env(self):
@@ -218,7 +128,8 @@ class glusterfs_eval_ovh(performing_actions):
 
         self.nodes = provisioner.nodes
         self.hosts = provisioner.hosts
-        node_ids_file = provisioner.node_ids_file
+        node_ids = provisioner.node_ids
+        driver = provisioner.driver
 
         self.data_nodes = list()
         self.clusters = dict()
@@ -246,8 +157,10 @@ class glusterfs_eval_ovh(performing_actions):
         if not self.args.no_config_host:
             self.config_host()
 
+        self.args.node_ids_file = None
+
         logger.info("FINISH SETTING THE EXPERIMENT ENVIRONMENT\n")
-        return node_ids_file
+        return node_ids, driver
 
     def create_configs(self):
         n_nodes_per_cluster = max(self.normalized_parameters['n_nodes_per_dc'])
@@ -275,7 +188,7 @@ class glusterfs_eval_ovh(performing_actions):
         self.create_configs()
 
         logger.info('''Your largest topology:
-                        gluster DCs: %s
+                        GlusterFS DCs: %s
                         n_gluster_per_dc: %s''' % (
                                                     len(self.configs['exp_env']['clusters']),
                                                     max(self.normalized_parameters['n_nodes_per_dc'])
@@ -287,18 +200,31 @@ class glusterfs_eval_ovh(performing_actions):
                                       parameters=self.normalized_parameters)
 
         gluster_volume_name = 'gluster_volume'
-        node_ids_file = None
+        gluster_mountpoint = '/mnt/glusterd-$(hostname)'
+        node_ids = None
         while len(sweeper.get_remaining()) > 0:
-            if node_ids_file is None:
-                node_ids_file = self.setup_env()
+            if node_ids is None:
+                node_ids, driver = self.setup_env()
             comb = sweeper.get_next()
             sweeper = self.run_exp_workflow(comb=comb,
                                             sweeper=sweeper,
+                                            gluster_mountpoint=gluster_mountpoint,
                                             gluster_volume_name=gluster_volume_name)
-            # if not is_nodes_alive(node_ids_file):
-            #     node_ids_file = None
+            logger.info('==================================================')
+            logger.info('Checking whether all provisioned nodes are running')
+            is_nodes_alive, _ = is_node_active(node_ids=node_ids, 
+                                               project_id=self.configs['project_id'], 
+                                               driver=driver)
+            if not is_nodes_alive:
+                logger.info('Deleting old provisioned nodes')
+                delete_ovh_nodes(node_ids=node_ids,
+                                project_id=self.configs['project_id'], 
+                                driver=driver)
+                node_ids = None
         logger.info('Finish the experiment!!!')
-
+        delete_ovh_nodes(node_ids=node_ids,
+                                project_id=self.configs['project_id'], 
+                                driver=driver)
 
 if __name__ == "__main__":
     logger.info("Init engine in %s" % __file__)
